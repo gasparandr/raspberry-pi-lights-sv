0d5735bdacda62d74e179b2ceb13d654c4bcf7a2
Andrei Gaspar <andrei@ildiesign.com>
2018-11-29 19:34:14 +0200

    - Initial boiler plate

---
+++ src/Server.ts
@@ -0,0 +1,64 @@
+
+
+require( "dotenv" ).config();
+
+
+import * as bodyParser from "body-parser";
+import * as express from "express";
+import * as ejs from "ejs";
+
+
+const publicPath = __dirname.substr( 0, __dirname.indexOf( "build" ) ) + "public";
+
+
+class Server {
+
+    public app: express.Application;
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+        this.errors();
+    }
+
+
+
+    public config() {
+
+
+        this.app.set( "view engine", "ejs" );
+
+        this.app.use( "*/public", express.static( publicPath ) );
+
+
+        this.app.use( bodyParser.urlencoded( { extended: true } ) );
+        this.app.use( bodyParser.json() );
+
+
+    }
+
+
+
+    public routes() {
+        let router: express.Router;
+        router = express.Router();
+
+        this.app.use( '/', router );
+
+
+    }
+
+
+
+    public errors() {
+        this.app.use( (err, req, res, next) => {
+            res.status( 422 ).json( { success: false, message: err.message } );
+        });
+    }
+
+}
+
+
+
+export default new Server().app;
