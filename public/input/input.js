9b2e0ad7936473f511a428efec0131c112cee262
Andrei Gaspar <andrei@ildiesign.com>
2018-12-02 21:45:22 +0200

    - Light state off styles

--- public/input/input.js
+++ public/input/input.js
@@ -9,3 +9,32 @@ $("#range-input").roundSlider({
     showTooltip: false,
     value: 50
 });
+
+
+var container       = document.getElementById( "input-screen-container" );
+var btnContainer    = document.getElementById( "button-container" );
+var rangeContainer  = document.getElementById( "range-input-container" );
+var switchButton    = document.getElementById( "switch-btn" );
+
+
+
+switchButton.addEventListener( "click", function () {
+
+    if ( switchButton.classList.contains( "button-off" ) ) {
+
+        rangeContainer.classList.remove( "hide" );
+        container.classList.remove( "background-off" );
+        btnContainer.classList.remove( "off" );
+        switchButton.classList.remove( "button-off" );
+        switchButton.innerHTML = "ON";
+
+    } else {
+
+        rangeContainer.classList.add( "hide" );
+        container.classList.add( "background-off" );
+        btnContainer.classList.add( "off" );
+        switchButton.classList.add( "button-off" );
+        switchButton.innerHTML = "OFF";
+
+    }
+});
