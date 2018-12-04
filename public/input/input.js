0e84412e6ffe35b3d4760b0c92b6ca501837aa88
Andrei Gaspar <andrei@ildiesign.com>
2018-12-04 22:57:39 +0200

    - Off state background animation

--- public/input/input.js
+++ public/input/input.js
@@ -15,7 +15,10 @@ var container       = document.getElementById( "input-screen-container" );
 var btnContainer    = document.getElementById( "button-container" );
 var rangeContainer  = document.getElementById( "range-input-container" );
 var switchButton    = document.getElementById( "switch-btn" );
+var background      = document.getElementById( "background" );
 
+
+var animation;
 
 
 switchButton.addEventListener( "click", function () {
@@ -28,6 +31,15 @@ switchButton.addEventListener( "click", function () {
         switchButton.classList.remove( "button-off" );
         switchButton.innerHTML = "ON";
 
+        background.classList.add( "disable-css-transitions" );
+
+        background.classList.remove( "light" );
+        if ( animation ) {
+            clearTimeout( animation );
+        }
+
+
+
     } else {
 
         rangeContainer.classList.add( "hide" );
@@ -35,6 +47,13 @@ switchButton.addEventListener( "click", function () {
         btnContainer.classList.add( "off" );
         switchButton.classList.add( "button-off" );
         switchButton.innerHTML = "OFF";
+
+
+        background.classList.remove( "disable-css-transitions" );
+
+        animation = setTimeout( function () {
+            background.classList.add( "light" );
+        }, 100 );
 
     }
 });
